9c166191e9007c5a92868484dc0495e4f5c01137
João Emanoel Mendonça Matos <141441658+JoaoEmanoelM@users.noreply.github.com>
2024-09-12 15:44:18 -0300

    Update jogoCartas.php

--- aula 7/jogoCartas.php
+++ aula 7/jogoCartas.php
@@ -1,5 +1,58 @@
 <?php 
 
-class Cartas {
+class Carta {
     
+    private $naipe;
+    private $numero;
+
+    
+    /**
+     * Get the value of naipe
+     */
+    public function getNaipe()
+    {
+        return $this->naipe;
+    }
+
+    /**
+     * Set the value of naipe
+     */
+    public function setNaipe($naipe): self
+    {
+        $this->naipe = $naipe;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numero
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set the value of numero
+     */
+    public function setNumero($numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
 }
+
+$naipes = ["Copas", "Espadas", "Ouros" , "Paus"];
+$numeros = ["Ás", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Valete", "Rainha", "Rei"];
+
+foreach($naipes as $i => $nai){
+    foreach($numeros as $i => $num){
+        $carta = new Carta;
+        $carta->setNumero($num);
+        $carta->setNaipe($nai);
+        $cartas[] = $carta;
+    }
+}
+
